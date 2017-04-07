ab38c445ca135f4d96fab0988082a01f33d9049c
marrow <marrow1203@gmail.com>
2017-04-07 17:57:07 +0800

    sctpclient01 done

--- Part-10/sctpclient01.c
+++ Part-10/sctpclient01.c
@@ -1,5 +1,8 @@
 #include "unp.h"
 
+#define SCTP_MAXLINE 800
+#define SERV_MAX_SCTP_STRM 10
+
 void sctpstr_cli(FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen)
 {
     struct sockaddr_in peeraddr;
@@ -30,6 +33,43 @@ void sctpstr_cli(FILE *fp, int sock_fd, struct sockaddr *to, socklen_t tolen)
     }
 }
 
+void sctpstr_cli_echoall(FILE *fp, int sock_fd, struct sockaddr *to,
+                        socklen_t tolen)
+{
+    struct sockaddr_in peeraddr;
+    struct sctp_sndrcvinfo sri;
+    char sendline[SCTP_MAXLINE], recvline[SCTP_MAXLINE];
+    socklen_t len;
+    int rd_sz, i, strsz;
+    int msg_flags;
+
+    bzero(sendline, sizeof(sendline));
+    bzero(&sri, sizeof(sri));
+
+    while (fgets(sendline, SCTP_MAXLINE - 9, fp) != NULL) {
+        strsz = strlen(sendline);
+        if (sendline[strsz - 1] == '\n') {
+            sendline[strsz - 1] = '\0';
+            strsz--;
+        }
+        for (i=0; i<SERV_MAX_SCTP_STRM; i++) {
+            snprintf(sendline + strsz, sizeof(sendline) - strsz,
+                    ".msg.%d", i);
+            Sctp_sendmsg(sock_fd, sendline, sizeof(sendline),
+                        to, tolen, 0, 0, i, 0, 0);
+        }
+        for (i=0; i<SERV_MAX_SCTP_STRM; i++) {
+            len = sizeof(peeraddr);
+            rd_sz = sctp_recvmsg(sock_fd, recvline, sizeof(recvline),
+                                (SA *) &peeraddr, &len, &sri, &msg_flags);
+            printf("From str:%d seq:%d (assoc:0x%x):",
+                    sri.sinfo_stream, sri.sinfo_ssn,
+                    (u_int)sri.sinfo_assoc_id);
+            printf("%.*s\n", rd_sz, recvline);
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int sock_fd;
@@ -56,9 +96,9 @@ int main(int argc, char const *argv[])
     Setsockopt(sock_fd, IPPROTO_SCTP, SCTP_EVENTS, &evnts, sizeof(evnts));
     if (echo_to_all == 0)
         sctpstr_cli(stdin, sock_fd, (SA *) &servaddr, sizeof(servaddr));
-    // else
-    //     sctpstr_cli_echoall(stdin, sock_fd, (SA *) &servaddr,
-    //                         sizeof(servaddr));
+    else
+        sctpstr_cli_echoall(stdin, sock_fd, (SA *) &servaddr,
+                            sizeof(servaddr));
     Close(sock_fd);
     return 0;
 }
